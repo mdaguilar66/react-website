0733397b2f7d924c79ff1209864d18ad73aa00be
Molly d'Aguilar <mdaguilar66@gmail.com>
2023-03-23 08:21:29 +0000

    json file for photo entries

--- src/components/AlbumMaker.js
+++ src/components/AlbumMaker.js
@@ -1,18 +1,20 @@
 import React from "react";
 import PhotoEntry from "./PhotoEntry";
 import './master.css';
+import entries from './data/photo-entries';
 
 class AlbumMaker extends React.Component {
     render() {
+
+        console.log(entries)
+
         return (
             <div className="album-maker">
-                <PhotoEntry src="red-living-room.jpeg" caption="A funky red living room" location="London UK" />
-                <PhotoEntry src="red-living-room.jpeg" caption="A funky red living room" location="London UK" />
-                <PhotoEntry src="red-living-room.jpeg" caption="A funky red living room" location="London UK" />
-                <PhotoEntry src="red-living-room.jpeg" caption="A funky red living room" location="London UK" />
-                <PhotoEntry src="red-living-room.jpeg" caption="A funky red living room" location="London UK" />
-                <PhotoEntry src="red-living-room.jpeg" caption="A funky red living room" location="London UK" />
-                <PhotoEntry src="red-living-room.jpeg" caption="A funky red living room" location="London UK" />
+                {
+                    entries.itemList.map((photo) => {
+                        return <PhotoEntry key={photo.src} src={photo.src} caption={photo.caption} location={photo.location} />
+                    })
+                }
             </div>
         )
     }
